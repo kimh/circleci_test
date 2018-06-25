8a2cebf4434b3d9317e07d83d3ab6619a0c6c0f3
kurumai <kurumai@jp.fujitsu.com>
2018-06-25 15:35:10 +0900

    initial commit

---
+++ src/main/java/circleci_test/mypkg/App.java
@@ -0,0 +1,13 @@
+package circleci_test.mypkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
